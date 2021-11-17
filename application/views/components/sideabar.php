7afccc23b2dac38f618602323015299f44cebcdf
Dardan2021 <78737429+Dardan2021@users.noreply.github.com>
2021-11-17 20:05:01 +0100

    profile first commit

--- application/views/components/sideabar.php
+++ application/views/components/sideabar.php
@@ -4,7 +4,6 @@
             <?php echo anchor("#", '<span class="profileFirst"><img src="'.BASE_URL.'assets/images/background.jpg" alt="" class="imgProfile"></span>');?>
         </li>
         <li>
-
             <?php echo "<a href=''><span class='icon .icon-users'>&#128100</span>".self::getSession("name")."</a>"?>
         </li>
         <li>
